fb0daf406cd99f70eb98cb8fdb79d2ec0de443de
Juha Palmu <juhamikaelpalmu@gmail.com>
2026-05-11 21:25:22 +0300

    Add tide config to bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,4 @@
 
 stow --target=$HOME nvim kitty fish
 
+tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Dark --show_time='24-hour format' --classic_prompt_separators=Vertical --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
