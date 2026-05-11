90c1fdb8e3cf5d3dba8e1e6d1e92f5fb62910d85
Juha Palmu <juhamikaelpalmu@gmail.com>
2026-05-11 22:37:52 +0300

    Set docker alias in config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -U fish_prompt_pwd_dir_length 0
 set -g __fish_git_prompt_show_status 1
 
+alias podman=docker 
+
 fish_add_path ~/.cargo/bin
 
 # overwrite greeting
