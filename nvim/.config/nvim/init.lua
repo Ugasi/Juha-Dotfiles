fe521f913a6de2e626755388365a9986d694d856
Juha Palmu <juhamikaelpalmu@gmail.com>
2026-05-23 03:45:23 +0300

    Change how oil options are passed

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,23 +9,6 @@ require('config.kickstart')
 -- Package manager configuration
 require('config.lazy')
 
-require('oil').setup({
-    default_file_explorer = true,
-    delete_to_trash = true,
-    skip_confirm_for_simple_edits = true,
-
-    view_options = {
-        show_hidden = true,
-        natural_order = true,
-        is_always_hidden = function(name, _)
-            return name == '..' or name == '.git'
-        end
-    },
-    win_options = {
-        wrap = true,
-    }
-})
-
 -- Theme configuration
 vim.cmd[[colorscheme tokyonight]]
 
--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,22 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+
+    view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+            return name == '..' or name == '.git'
+        end
+    },
+    win_options = {
+        wrap = true,
+    }
+  },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
