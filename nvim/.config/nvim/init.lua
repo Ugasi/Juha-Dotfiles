859aa3c910683b2cf7fe1a455d76b0862c2e44cc
Juha Palmu <juhamikaelpalmu@gmail.com>
2026-05-23 03:50:00 +0300

    Change how telescope options are used

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,9 +12,3 @@ require('config.lazy')
 -- Theme configuration
 vim.cmd[[colorscheme tokyonight]]
 
--- Telescope configuration
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,18 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
+  "nvim-telescope/telescope.nvim", version = '*',
+  dependencies = {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+  },
+  cmd = "Telescope",
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep (live)" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+  },
+  config = function()
+    require("telescope").setup({})
+  end,
 }
+
