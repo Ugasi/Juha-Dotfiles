90c2968bcc233714270da514215056c1b5ec553c
Juha Palmu <juhamikaelpalmu@gmail.com>
2026-06-05 01:51:27 +0300

    Add a single place to list all used lsps

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,9 @@
+package.path = package.path .. ";../?.lua"
+
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = {}
+        ensure_installed = require("config.lsp-list") 
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
