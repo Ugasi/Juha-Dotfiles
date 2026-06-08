8f65552a6e781a21f7d360feec750c5133186384
Juha Palmu <juhamikaelpalmu@gmail.com>
2026-06-08 03:49:13 +0300

    Add tpm clone to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,6 @@ stow --target=$HOME nvim kitty fish tmux
 fisher update
 
 tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Dark --show_time='24-hour format' --classic_prompt_separators=Vertical --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
+
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
